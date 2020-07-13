3bbfd3c40c2d54ac0ad1eba5b233a12512722113
Andres <andresefr24@gmail.com>
2020-07-13 19:35:43 +0000

    latest readme additions and not found page layout

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Default from "~/pages/_layouts/Default";
+import * as S from "./styles";
+import { Button } from "~/components/Button";
+import Logo from "~/assets/Branding-mark@4x.png";
 
 const NotFound = () => {
-  return <div>Not found</div>;
+  return (
+    <Default>
+      <S.Container>
+        <img src={Logo} alt="logo" height={50} />
+        <S.Message>Not found</S.Message>
+        <Link to="/">
+          <Button type="primary">Go back</Button>
+        </Link>
+      </S.Container>
+    </Default>
+  );
 };
 
 export default NotFound;
